5a3fd7233959b2528c8a8ca03b21ff2d5ba50299
jiwonsudo <wondev112@gmail.com>
2022-02-12 02:33:46 +0900

    responseJSON으로 변경

--- DaeguExhibitonFinder/ViewController.swift
+++ DaeguExhibitonFinder/ViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
+    
+    let url = "https://dgfc.or.kr/ajax/event/list.json?event_gubun=DP&start_date=2021-07"
+    let param : Parameters = [
+        "년" : "",
+        "월" : ""
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        request("https://httpbin.org").response {
+        AF.request(url).responseJSON {
             response in debugPrint(response)
         }
     }
